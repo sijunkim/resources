1e4fde1aafaadecfdfc513a0900eba6c2a2156ea
김시준 <papaya9349@naver.com>
2024-06-29 16:44:46 +0900

    edit kotlin_init

--- mysql/initdb.d/kotlin_project/kotlin_init.sql
+++ mysql/initdb.d/kotlin_project/kotlin_init.sql
@@ -2,7 +2,10 @@ CREATE DATABASE IF NOT EXISTS `kotlin_project`;
 
 USE `kotlin_project`;
 
-CREATE TABLE IF NOT EXISTS users (
-  `ID` INT PRIMARY KEY,
-	`EXTRA_DATA` VARCHAR(200) NOT NULL
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `extra_data` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
